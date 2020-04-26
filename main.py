7734c4107f8b99502a8883b12bf8931c1a414eeb
jamie <jamie@JonPC>
2020-04-26 02:00:04 +0100

    refactor main

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from src import apache_links
 
 class Args(object):
     def __init__(self, argv):
-        self.urls = filter(lambda arg: arg[0] != "-", argv[1:])
-
+        self.urls = [arg for arg in argv[1:] if arg[0] != "-"]
         try:
             _, args = getopt.getopt(argv, "n:a:")
             self.no_download = '-n' in args
@@ -22,31 +21,26 @@ class Args(object):
             self.no_download = False
             self.is_apache = False
 
+def main(args):
+    def get_strategy(url):
+        if args.is_apache:
+            match = apache_links
+        elif "khinsider" in url:
+            match = khinsider_links
+        else:
+            match = bandcamp_links
+        return match.AlbumDownloadStrategy(url)
 
-def get_strategy(url, args):
-    if args.is_apache:
-        match = apache_links
-    elif "khinsider" in url:
-        match = khinsider_links
-    else:
-        match = bandcamp_links
-
-    return match.AlbumDownloadStrategy(url)
-
-def main(argv):
-    args = Args(argv)
-    strategies = map(lambda url: get_strategy(url, args), args.urls)
-
-    for strategy in strategies:
+    def execute(strategy):
         album_name = strategy.album_name
         artist_name = strategy.artist
-
         if not args.no_download:
             if hasattr(strategy, "tracks"):
                 download.download_tracks(strategy.tracks, "mp3")
             else:
                 download.download_list(strategy.links, "mp3")
 
+    [execute(get_strategy(url)) for url in args.urls]
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main(Args(sys.argv))
